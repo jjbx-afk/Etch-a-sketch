a312db759faa32b362dc85ba52817521a85b7be8
Bayane Chakir <bayane.pbg@gmail.com>
2025-02-24 14:17:49 +0100

    optimiser le code et metrre tout le css dans une fiche css, tentative de supprimer ancienne grille si nouvelle génerer

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ title.style.textDecoration = "underline";
 
 let containerButton = document.createElement("div");
 document.body.appendChild(containerButton);
+containerButton.classList.add("btn-container"); 
 containerButton.style.textAlign = "center";
 containerButton.style.display = "flex";
 containerButton.style.gap = "3%";
@@ -34,287 +35,104 @@ containerButton.style.justifyContent = "center";
 
 const four = document.createElement("button");
 four.textContent = "4x4";
-four.classList.add("button");
+four.classList.add("btn");
 containerButton.appendChild(four);
 
 //8x8
 
 const eight = document.createElement("button");
 eight.textContent = "8x8";
-eight.classList.add("button");
+eight.classList.add("btn");
 containerButton.appendChild(eight);
 
 //12x12
 
 const twelve = document.createElement("button");
 twelve.textContent = "12x12";
-twelve.classList.add("button");
+twelve.classList.add("btn");
 containerButton.appendChild(twelve);
 
 //16x16
 
 const sixteen = document.createElement("button");
 sixteen.textContent = "16x16";
-sixteen.classList.add("button");
+sixteen.classList.add("btn");
 containerButton.appendChild(sixteen);
 
-//Trash can
+// trash button 
 const trash = document.createElement("button");
 trash.textContent = "🗑️";
-trash.classList.add("button");
+trash.classList.add("btn");
 containerButton.appendChild(trash);
 
 
 
 // event listener to make the button work 
 
-four.addEventListener("click", btnFour);
-eight.addEventListener("click", btnEight);
-twelve.addEventListener("click", btnTwelve);
-sixteen.addEventListener("click", btnSixteen);
+four.addEventListener("click", () => drawGrid(4));
+eight.addEventListener("click", () => drawGrid(8));
+twelve.addEventListener("click", () => drawGrid(12));
+sixteen.addEventListener("click", () => drawGrid(16));
 
+// trash.addEventListener("click", trash)
 
-// button class style
-four.style.alignItems = "enter";
-four.style.backgroundColor = "#fee6e3";
-four.style.borderRadius = "8px";
-four.style.cursor = "pointer";
-four.style.fontFamily = "Inter,sans-serif";
-four.style.fontSize = "10px";
-four.style.height = "28px";
-four.style.justifyContent = "center";
-four.style.maxWidth = "100%";
-four.style.padding = "0 25px";
-four.style.position = "relative";
-four.style.textAlign = "center";
-four.style.textDecoration = "none";
-
-eight.style.alignItems = "enter";
-eight.style.backgroundColor = "#fee6e3";
-eight.style.borderRadius = "8px";
-eight.style.cursor = "pointer";
-eight.style.fontFamily = "Inter,sans-serif";
-eight.style.fontSize = "10px";
-eight.style.height = "28px";
-eight.style.justifyContent = "center";
-eight.style.maxWidth = "100%";
-eight.style.padding = "0 25px";
-eight.style.position = "relative";
-eight.style.textAlign = "center";
-eight.style.textDecoration = "none";
-
-twelve.style.alignItems = "enter";
-twelve.style.backgroundColor = "#fee6e3";
-twelve.style.borderRadius = "8px";
-twelve.style.cursor = "pointer";
-twelve.style.fontFamily = "Inter,sans-serif";
-twelve.style.fontSize = "10px";
-twelve.style.height = "28px";
-twelve.style.justifyContent = "center";
-twelve.style.maxWidth = "100%";
-twelve.style.padding = "0 25px";
-twelve.style.position = "relative";
-twelve.style.textAlign = "center";
-twelve.style.textDecoration = "none";
-
-sixteen.style.alignItems = "enter";
-sixteen.style.backgroundColor = "#fee6e3";
-sixteen.style.borderRadius = "8px";
-sixteen.style.cursor = "pointer";
-sixteen.style.fontFamily = "Inter,sans-serif";
-sixteen.style.fontSize = "10px";
-sixteen.style.height = "28px";
-sixteen.style.justifyContent = "center";
-sixteen.style.maxWidth = "100%";
-sixteen.style.padding = "0 25px";
-sixteen.style.position = "relative";
-sixteen.style.textAlign = "center";
-sixteen.style.textDecoration = "none";
-
-// make a button to clear the grid 
-
-// creats a container for the grid/cells
-
-let containerDiv = document.createElement("div");
-document.body.appendChild(containerDiv);
 
 containerDiv.classList.add('container');
 
-containerDiv.style.width = "346px";
-containerDiv.style.marginInline = "auto";
-containerDiv.style.marginBlock = "150px";
+const containerDiv = document.createElement("div");
+document.body.appendChild(containerDiv);
+webkitURL
 
 
 
 
-// 4 Function to make the grids
+
+//Function to make the grids
 
 
-function btnFour() {
-    for (let i = 0; i < 4; i++) {
+function drawGrid(size, width) {
+
+    let newGrid = document.createElement("div");
+    
+
+    for (let i = 0; i < size; i++) {
 
         let newDiv = document.createElement("div");
-        containerDiv.appendChild(newDiv);
+        newGrid.appendChild(newDiv);
 
         newDiv.classList.add('grid-line');
         newDiv.style.display = "flex";
         newDiv.style.marginInline = "auto";
 
-        for (let i = 0; i < 4; i++) {
+        for (let i = 0; i < size; i++) {
 
             let innerNewDiv = document.createElement("div");
 
             newDiv.appendChild(innerNewDiv);
 
             innerNewDiv.classList.add('grid-line');
-            innerNewDiv.style.display = "flex";
-            innerNewDiv.style.height = "20px";
-            innerNewDiv.style.width = "20px";
-            innerNewDiv.style.border = "1px solid black";
-            innerNewDiv.style.clear = "both";
-
-
             innerNewDiv.addEventListener('mouseover', () => {
-                innerNewDiv.style.backgroundColor = "#eec0c8";
+                innerNewDiv.classList.add('mouse-over');
             });
 
         };
     };
-}
-function btnEight() {
-    for (let i = 0; i < 8; i++) {
 
-        let newDiv = document.createElement("div");
-        containerDiv.appendChild(newDiv);
-
-        newDiv.classList.add('grid-line');
-
-        newDiv.style.display = "flex";
-
-        for (let i = 0; i < 8; i++) {
-
-            let innerNewDiv = document.createElement("div");
-
-            newDiv.appendChild(innerNewDiv);
-
-            innerNewDiv.classList.add('grid-line');
-
-            innerNewDiv.style.display = "flex";
-
-            innerNewDiv.style.height = "20px";
-            innerNewDiv.style.width = "20px";
-            innerNewDiv.style.border = "1px solid black";
-            innerNewDiv.style.clear = "both";
-
-            innerNewDiv.addEventListener('mouseover', () => {
-                innerNewDiv.style.backgroundColor = "#eec0c8";
-            });
-
-        };
-    };
-}
-function btnTwelve() {
-    for (let i = 0; i < 12; i++) {
-
-        let newDiv = document.createElement("div");
-        containerDiv.appendChild(newDiv);
-
-        newDiv.classList.add('grid-line');
-
-        newDiv.style.display = "flex";
-
-        for (let i = 0; i < 12; i++) {
-
-            let innerNewDiv = document.createElement("div");
-
-            newDiv.appendChild(innerNewDiv);
-
-            innerNewDiv.classList.add('grid-line');
-
-            innerNewDiv.style.display = "flex";
-            innerNewDiv.style.height = "20px";
-            innerNewDiv.style.width = "20px";
-            innerNewDiv.style.border = "1px solid black";
-            innerNewDiv.style.clear = "both";
-
-            innerNewDiv.addEventListener('mouseover', () => {
-                innerNewDiv.style.backgroundColor = "#eec0c8";
-            });
-
-        };
-    };
-}
-
-
-function btnSixteen() {
-    for (let i = 0; i < 16; i++) {
-
-        let newDiv = document.createElement("div");
-        containerDiv.appendChild(newDiv);
-
-        newDiv.classList.add('grid-line');
-
-        newDiv.style.display = "flex";
-
-        for (let i = 0; i < 16; i++) {
-
-            let innerNewDiv = document.createElement("div");
-
-            newDiv.appendChild(innerNewDiv);
-
-            innerNewDiv.classList.add('grid-line');
-
-            innerNewDiv.style.display = "flex";
-            innerNewDiv.style.height = "20px";
-            innerNewDiv.style.width = "20px";
-            innerNewDiv.style.border = "1px solid black";
-            innerNewDiv.style.clear = "both";
-
-            innerNewDiv.addEventListener('mouseover', () => {
-                innerNewDiv.style.backgroundColor = "#eec0c8";
-            });
-
-        };
-    };
-}
-
-
-document.innerHTML = function conditionBtn() {
-
-
-if (btnFour.clicked == true) {
-
-    btnFour();
-}
-if (btnEight.clicked == true) {
-    btnEight();
-}
-if (btnTwelve.clicked == true) {
-    btnTwelve();
-}
-if (btnSixteen.clicked == true) {
-    btnSixteen();
-}
+    containerDiv.replaceChildren(newGrid);
 };
-// make a condition for trash can if clicked 
-
-conditionBtn();
 
 
 
 
-// to add : 
-
-// trash can to delete drawing
-
-// center grids x
-
-// when another button clicked delete the past grid :
-// use inner html empty the container and replace with the clicked function 
-//  use the name of the var. innerHTML = the condition.
-
-
-// make a footer 
-
-
-
+    // if (btnFour.clicked == true) {
+    //     containerDiv.replaceChildren(drawGrid());
+    // }
+    // if (btnEight.clicked == true) {
+    //     containerDiv.replaceChildren(drawGrid());
+    // }
+    // if (btnTwelve.clicked == true) {
+    //     containerDiv.replaceChildren(drawGrid());
+    // }
+    // if (btnSixteen.clicked == true) {
+    //     containerDiv.replaceChildren(drawGrid());
+    // }
